f31f6095e3dcb7c7bbebf4d0692d44a1b9bf02e3
kiba gateaux <kibagateaux@gmail.com>
2018-11-30 19:01:25 +0100

    adds cors to api calls

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,20 @@
 // server.js
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import 'babel-polyfill';
 import Stats from './src/controllers/Stats';
 
 dotenv.config();
 const app = express()
 
+app.use(cors());
 app.use(express.json())
 
 app.get('/', (req, res) => {
   return res.status(200).send({'message': 'YAY! Congratulations! Your first endpoint is working'});
 });
+
 app.get('/api/addresscount', Stats.getAllAddressCount);
 app.get('/api/avggasprice', Stats.getAllAvgGasPrice);
 app.get('/api/etherprice', Stats.getAllEtherPrice);
